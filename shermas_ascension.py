9dc1f9c51c5c4dd660850059e15c106d761ea88d
JeremieD-B <j.dufour.blancke@etu.imt-nord-europe.fr>
2025-12-17 16:35:06 +0100

    Update shermas_ascension.py

--- shermas_ascension.py
+++ shermas_ascension.py
@@ -5,7 +5,7 @@
 # Diagramme activité ligne 1932 : Caverne des Cloches 
 # Lignes ~1400 - ~2100 = Fonctions de salle 
 # Dernière ligne : exécution du jeu
-# Pour les test ne pas hésiter à activer la fonction triche
+# Pour les test ne pas hésiter à activer la fonction triche (ligne commenté dans la fonction jouer à la fin du programme)
 
 
 ##### Imports
